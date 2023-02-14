f22a54fa5b592078e7689c3ef9474dcb23fbb003
alexrose1 <alex.rose1@kellogg.northwestern.edu>
2023-02-14 23:36:19 +0000

    labs1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,24 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+
   end
 
+  def show
+    @posts = Post.find_by({ "id" => params["id"] })
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.new
+     @posts["author"] = params["post"]["author"]
+     @posts["body"] = params["post"]["body"]
+     @posts["image"] = params["post"]["image"]
+     @posts.save
+    
+
+  end
 end
